9ba74231697060db3ece302b6a30ab9d9865e07c
Yorwba <yorwb4@gmail.com>
2024-10-27 14:02:33 +0100

    Add AGPL

--- packed_db.py
+++ packed_db.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+#   dic.html puts simple dictionary search into one self-contained HTML file.
+#   Copyright 2024 Yorwba
+
+#   dic.html is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU Affero General Public License
+#   as published by the Free Software Foundation, either version 3 of
+#   the License, or (at your option) any later version.
+
+#   dic.html is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+
+#   You should have received a copy of the GNU Affero General Public License
+#   along with dic.html.  If not, see <https://www.gnu.org/licenses/>.
+
 import html
 import pathlib
 import sqlite3
